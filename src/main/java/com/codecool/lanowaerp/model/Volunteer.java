849ad69be8a4c13cbc590534a1a6ca259990c484
Marek Stopka <keramakpots@gmail.com>
2017-10-18 10:19:44 +0200

    Move volunteer to model package

---
+++ src/main/java/com/codecool/lanowaerp/model/Volunteer.java
@@ -0,0 +1,57 @@
+package com.codecool.lanowaerp.model;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Volunteer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column
+    @NotNull
+    private String name;
+    @Column
+    @NotNull
+    private String surname;
+    @Column
+    private String telephone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
