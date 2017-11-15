d7e36fed3a6f8b372fbc955e7e8dda06d045a3ef
Marek Stopka <keramakpots@gmail.com>
2017-11-15 18:11:24 +0100

    Make model change for adding nessesery fields for register handling

--- src/main/java/com/codecool/lanowaerp/model/Meeting.java
+++ src/main/java/com/codecool/lanowaerp/model/Meeting.java
@@ -4,14 +4,16 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
-import org.springframework.data.annotation.Id;
+
 
 @Entity
 public class Meeting {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column
@@ -19,4 +21,19 @@ public class Meeting {
     private Date date;
 
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
